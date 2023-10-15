05aaa600ab5ebd67eb554b44024d6fc9a5b84395
DaniloAlmeidaSantos <danilo8almeida@hotmail.com>
2023-10-15 16:21:39 -0300

    Creating screen to login

---
+++ src/services/User.service..ts
@@ -0,0 +1,9 @@
+import AuthenticateRequestModel from '../models/AuthenticateRequest.model';
+import AuthenticateResponseModel from '../models/AuthenticateResponse.model';
+
+class UserService {
+    
+    static authenticateUser(request: AuthenticateRequestModel ) {
+        
+    }
+}
