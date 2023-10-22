ca6a617ba232ac50112d74a89404833ff73e2589
DaniloAlmeidaSantos <danilo8almeida@hotmail.com>
2023-10-22 15:33:11 -0300

    Improving new components and styles to home

--- src/components/menu/menu.component.tsx
+++ src/components/menu/menu.component.tsx
@@ -1,12 +1,31 @@
-import './menu.component.css';
+import './menu.style.css';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHome, faCircleInfo, faChartLine, faListCheck, faUser } from "@fortawesome/free-solid-svg-icons";
 
-function Menu() {
+function Menu(props: any) {
+
+    const { selectedItem } = props;
 
     return (
         <div className='footer-menu'>
 
             <section className='menu-items'>
+                <a href="/" className='menu-icon'>
+                    <FontAwesomeIcon className={selectedItem === 'graphs' ? 'selected-item' : 'not-selected-item'} size="3x" icon={faChartLine} color='#000' />
+                </a>
 
+                <a href="/" className='menu-icon'>
+                    <FontAwesomeIcon size={selectedItem === 'info' ? '2x' : '3x'} className={selectedItem === 'info' ? 'selected-item' : 'not-selected-item'} icon={faCircleInfo} color='#000' />
+                </a>
+                <a href="/" className='menu-icon'>
+                    <FontAwesomeIcon size={selectedItem === 'home' ? '2x' : '3x'} className={selectedItem === 'home' ? 'selected-item' : 'not-selected-item'} icon={faHome} color='#000' />
+                </a>
+                <a href="/" className='menu-icon'>
+                    <FontAwesomeIcon size={selectedItem === 'goals'? '2x' : '3x'} className={selectedItem === 'goals'? 'selected-item' : 'not-selected-item'} icon={faListCheck} color='#000' />
+                </a>
+                <a href="/" className='menu-icon'>
+                    <FontAwesomeIcon size={selectedItem === 'user'? '2x' : '3x'} className={selectedItem === 'user'? 'selected-item' : 'not-selected-item'} icon={faUser} color='#000' />
+                </a>
             </section>
 
         </div>
--- src/screens/home/home-dashboard/home-dashboard.screen.tsx
+++ src/screens/home/home-dashboard/home-dashboard.screen.tsx
@@ -13,8 +13,27 @@ function App() {
             <Header />
             <div className='home-container'>
                 <h2>{HoursUtils.getGreetingMessage("Danilo")}</h2>
+                <div className='graph-box'>
+                    <section className='suggestion-debts'>
+                    </section>
+                    <section className='graph-debts'>
+                        
+                    </section>
+                    <section className='debts-per-months'>
+
+                    </section>
+                </div>
+
+                <div className='goals-and-legends-box'>
+                    <section className='legends-box'>
+
+                    </section>
+                    <section className='goals-box'>
+
+                    </section>
+                </div>
             </div>
-            <Menu />
+            <Menu selectedItem="home" />
         </>
 
     );
