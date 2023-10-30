da8cb564aa9892b9246589b24fab6e5dec0a6af5
DaniloAlmeidaSantos <danilo8almeida@hotmail.com>
2023-10-29 21:50:26 -0300

    Adding chart in homepage

--- src/screens/home/home-dashboard/home-dashboard.screen.tsx
+++ src/screens/home/home-dashboard/home-dashboard.screen.tsx
@@ -2,11 +2,29 @@ import { useState } from 'react';
 import HoursUtils from '../../../util/hours.util';
 import Header from '../../../components/header/header.component';
 import Menu from '../../../components/menu/menu.component';
+import { Chart } from "react-google-charts";
 import './home-dashboard.style.css';
 
 function App() {
 
     const [userInformation, setUserInformation] = useState({});
+
+    const data = [
+        ["Mês", "Gastos", "Orçamento do Mês"],
+        ["Janeiro", 1000.9, 100000],
+        ["Fevereiro", 23450.00, 100000],
+        ["Março", 660, 100000],
+        ["Abril", 1030, 100000],
+    ];
+
+    const options = {
+        chart: {
+            title: "Suas despesas nos últimos meses:",
+            subtitle: "Acompanhe seus gastos no seu último seis meses.",
+        },
+        colors: ['#ED7D31', '#4472C4', '#FFBB71', '#A5A5A5'],
+        xAxis: {format: 'decimal'}
+    };
 
     return (
         <>
@@ -15,12 +33,31 @@ function App() {
                 <h2>{HoursUtils.getGreetingMessage("Danilo")}</h2>
                 <div className='graph-box'>
                     <section className='suggestion-debts'>
+                        <Chart
+                            width={'100%'}
+                            height={'100%'}
+                            chartType="PieChart"
+                            data={data}
+                            options={options}
+                        />
                     </section>
                     <section className='graph-debts'>
-                        
+                        <Chart
+                            width={'100%'}
+                            height={'100%'}
+                            chartType="PieChart"
+                            data={data}
+                            options={options}
+                        />
                     </section>
                     <section className='debts-per-months'>
-
+                        <Chart
+                            chartType="Bar"
+                            width="100%"
+                            height="100%"
+                            data={data}
+                            options={options}
+                        />
                     </section>
                 </div>
 
