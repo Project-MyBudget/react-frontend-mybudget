4a52bfac73845d339c77e5564e66cc35256161db
GuilhermeMonteiroLins <guilins777@gmail.com>
2023-11-04 23:05:22 -0300

    implementation first stage in register user

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import AuthenticateScreen from './screens/user/authenticate/authenticate.screen.tsx';
+import RegisterScreen from './screens/user/register/first-stage-register.screen.tsx';
+import RegisterScreen2 from './screens/user/register/second-stage-register.screen.tsx';
 import HomeDashboard from './screens/home/home-dashboard/home-dashboard.screen.tsx';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <HomeDashboard />
+    <RegisterScreen/>
+    <RegisterScreen2/>
   </React.StrictMode>,
 );
---
+++ src/models/RegisterRequest.model.ts
@@ -0,0 +1,14 @@
+interface RegisterRequest{
+    firstName: string,
+    lastName: string,
+    dateOfBirth: string,
+    phoneNumber: string,
+    email: string,
+    password: string,
+    childrenNumber: number,
+    status: string,
+    gender: string,
+    civilStatus: string
+}
+
+export default RegisterRequest;
