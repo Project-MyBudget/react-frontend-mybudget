4f766a6e89db3f258e2da5a4ab7a343b3188566a
DaniloAlmeidaSantos <danilo8almeida@hotmail.com>
2023-11-09 00:05:20 -0300

    Fixing adjust register screen

--- src/screens/user/register/first-stage-register.screen.tsx
+++ src/screens/user/register/first-stage-register.screen.tsx
@@ -24,6 +24,7 @@ function App() {
         civilStatus: ''
     });
 
+
     const handleSubmit = async (e: any) => {
         e.preventDefault();
 
@@ -48,8 +49,8 @@ function App() {
                         src={logo}
                         alt="Logomarca da empresa My Budget, com as letras coma cor azul claro e um porco acima das letras." />
                 </section>
-                <div className="container-card">
-                    <section className="card-section">
+                <div className="container-card-register">
+                    <section className="card-section-register">
                         <section className="card-svg">
                             <img
                                 src={userIcon}
@@ -57,34 +58,41 @@ function App() {
                             <form className='register-form' onSubmit={handleSubmit}>
                                 <input type="text" name="firstName" placeholder='Nome' onChange={handleInputChange} value={formValues.firstName || ''} required maxLength={250} />
                                 <input type="text" name="lastName" placeholder='Sobrenome' onChange={handleInputChange} value={formValues.lastName || ''} required maxLength={250} />
-                                <input type="date" name="dateOfBirth" placeholder='Data de Nascimento' onChange={handleInputChange} value={formValues.dateOfBirth || ''} required maxLength={9} />
+                                <input className='date-of-register' type="date" name="dateOfBirth" placeholder='Data de Nascimento' onChange={handleInputChange} value={formValues.dateOfBirth || ''} required maxLength={9} />
                                 <input type="tel" name="phoneNumber" placeholder='Telefone' onChange={handleInputChange} value={formValues.phoneNumber || ''} required maxLength={11} />
                                 <input type="email" name="email" placeholder='E-mail' onChange={handleInputChange} value={formValues.email || ''} required maxLength={250} />
                                 <input type="password" name="password" placeholder='Senha' onChange={handleInputChange} value={formValues.password || ''} required maxLength={16} />
                                 <input type="password" name="repeatPassword" placeholder='Confirme sua senha' onChange={(e) => setRepeatPassword(e.target.value)} value={repeatPassword || ''} required maxLength={16} />
-                                <div>
-                                    <select name="civilStatus" placeholder='Estado civil' onChange={handleInputChange} value={formValues.civilStatus || ''} required>
-                                        <option value="SINGLE">Solteiro</option>
-                                        <option value="MARRIED">Casado</option>
-                                        <option value="DIVORCED">Divorciado</option>
-                                        <option value="WIDOWER">Viúvo</option>
-                                    </select>
-                                    <select name="childrenNumber" placeholder='Possui Filhos?' onChange={handleInputChange} value={formValues.civilStatus || ''} required>
-                                        <option value="SINGLE">Solteiro</option>
-                                        <option value="MARRIED">Casado</option>
-                                        <option value="DIVORCED">Divorciado</option>
-                                        <option value="WIDOWER">Viúvo</option>
-                                    </select>
-                                    <select name="civilStatus" placeholder='Estado civil' onChange={handleInputChange} value={formValues.civilStatus || ''} required>
-                                        <option value="SINGLE">Solteiro</option>
-                                        <option value="MARRIED">Casado</option>
-                                        <option value="DIVORCED">Divorciado</option>
-                                        <option value="WIDOWER">Viúvo</option>
-                                    </select>
-                                </div>
-                                <div className='button-area'>
-                                    <a href="/" className='btn-next'>Proximo</a>
-                                </div>
+                                <section className='group-check-box-first-stage'>
+                                    <label>
+                                        Status civil
+                                        <select name="civilStatus" placeholder='Estado civil' onChange={handleInputChange} value={formValues.civilStatus || ''} required>
+                                            <option value="SINGLE">Solteiro</option>
+                                            <option value="MARRIED">Casado</option>
+                                            <option value="DIVORCED">Divorciado</option>
+                                            <option value="WIDOWER">Viúvo</option>
+                                        </select>
+                                    </label>
+
+                                    <label>
+                                        Tem filhos?
+                                        <select name="childrenNumber" placeholder='Possui Filhos?' onChange={handleInputChange} value={formValues.civilStatus || 0} required>
+                                            <option value={0}>Não</option>
+                                            <option value={1}>Sim</option>
+                                        </select>
+                                    </label>
+
+                                    <label>
+                                        Gênero
+                                        <select name="civilStatus" placeholder='Gênero' onChange={handleInputChange} value={formValues.civilStatus || undefined} required>
+                                            <option value={"SINGLE"}>Solteiro</option>
+                                            <option value={"MARRIED"}>Casado</option>
+                                            <option value={"DIVORCED"}>Divorciado</option>
+                                            <option value={"WIDOWER"}>Viúvo</option>
+                                        </select>
+                                    </label>
+                                </section>
+                                <a href="/" className='btn-next'>Proximo</a>
                             </form>
                         </section>
                     </section>
