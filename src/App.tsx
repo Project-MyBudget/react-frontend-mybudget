c74c0880a089229edfd3a12aad6d867c236932b9
DaniloAlmeidaSantos <danilo8almeida@hotmail.com>
2023-11-13 23:00:55 -0300

    Fixing user-finace screen

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import RegisterFirstStepScreen from './screens/user/register/first-stage-registe
 import RegisterSecondStepScreen from './screens/user/register/second-stage-register.screen.tsx';
 import HomeDashboardScreen from './screens/home/home-dashboard/home-dashboard.screen.tsx';
 import InitialFinancialControl from './screens/financial-control/initial-financial-control.screen.tsx';
+import UserFinancial from './screens/financial-control/finances/user-finances.screen.tsx';
 
 function App() {
     return (
@@ -15,6 +16,7 @@ function App() {
                 <Route path="/register/step/initial" element={<RegisterFirstStepScreen />} />
                 <Route path="/register/step/confirm" element={<RegisterSecondStepScreen />} />
                 <Route path="/initial/financial-control" element={<InitialFinancialControl />} />
+                <Route path="/user/financial-control" element={<UserFinancial />} />
             </Routes>
         </>
     );
--- src/screens/financial-control/finances/user-finances.screen.tsx
+++ src/screens/financial-control/finances/user-finances.screen.tsx
@@ -4,58 +4,51 @@ import Menu from "../../../components/menu/menu.component";
 import BudgetComponent from '../../../components/budget/budget.component';
 import "./user-finances.style.css";
 
-import React from 'react';
-
-const BtnNewExpense = (props: any) => {
-    return (
-        <>
-            <div className="btn-nw-expense" onClick={() => props.onClick}>
-                <span>nova despesa</span>
-            </div>
-        </>
-    );
-}
-
-const CardExpenses = (props: any) => {
-    return (
-        <section>
-            <h1>{props.title}</h1>
-            <form className="frm-expense-types">
-                <div>
-                    <input type="date" name="date" placeholder="Data" />
-
-                    <select name="" id="expense-field">
-                        <option value="1">Despesa</option>
-                        <option value="1">Financeiro</option>
-                    </select>
-                    <input type="number" name="valor" min="0" placeholder="Valor" />
-                </div>
-                <BtnNewExpense />
-            </form>
-
-        </section>
-    )
-}
 
 const App = () => {
+
+    const InputCard = (props: any) => {
+        return (
+            <div className="frm-expense-types">
+                <input id="expense-field" type="date" name="date" placeholder="Data" />
+                <select id="expense-field">
+                    <option value="1">Despesa</option>
+                    <option value="1">Financeiro</option>
+                </select>
+                <input id="expense-field" type="number" name="valor" min="0" placeholder="Valor" />
+            </div>
+        );
+    };
+
+    const CardExpenses = (props: any) => {
+        return (
+            <section className="card-control-finance">
+                <h1>{props.title}</h1>
+                <form>
+                    <InputCard />
+                    <div className="btn-nw-expense" onClick={() => props.onClick}>
+                        <span>nova despesa</span>
+                    </div>
+                </form>
+            </section>
+        );
+    };
+
     return (
         <>
             <Header />
             <div className="user-finances-container">
-                <h1>Suas finanças</h1>
+                <h1 className="user-finance-title">Suas finanças</h1>
                 <BudgetComponent />
 
-                <main>
+                <main className="card-finances-control-area">
                     <CardExpenses title="Despesas essencias" />
-
                     <CardExpenses title="Despesas não essenciais" />
-
                     <CardExpenses title="Lazer" />
                 </main>
                 <div id="save">
                     <SaveButton />
                 </div>
-
             </div>
             <Menu />
         </>
